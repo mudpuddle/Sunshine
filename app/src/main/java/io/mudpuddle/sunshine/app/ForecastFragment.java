495dddc96a3b723b9f6c4fba1af7d8fae6dcdc5e
Jesse W <jesse.wegner@gmail.com>
2015-03-20 15:54:49 -0700

    working settings activity, with no actual settings

--- app/src/main/java/io/mudpuddle/sunshine/app/ForecastFragment.java
+++ app/src/main/java/io/mudpuddle/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package io.mudpuddle.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -91,6 +93,17 @@ public class ForecastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String weather = mForecastAdapter.getItem(position);
+                Intent detailIntent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, weather);
+                startActivity(detailIntent);
+
+
+            }
+        });
 
         return rootView;
     }
